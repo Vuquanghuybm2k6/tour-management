662253aafabd85a9f7d1a2b15d1b59c2458ef0d9
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-04-09 19:21:24 +0700

    Basic configuration

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express, {Express, Request, Response }from "express"
+const app: Express = express()
+const port:number = 3000
+app.get("/", (req:Request, res:Response)=>{
+  res.send("Trang chủ")
+})
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
