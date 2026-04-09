6913b6139d98344df37d9c26f3de2cb2705fadc5
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-04-09 19:34:16 +0700

    Config pug folder

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express, {Express, Request, Response }from "express"
 const app: Express = express()
 const port:number = 3000
-app.get("/", (req:Request, res:Response)=>{
-  res.send("Trang chủ")
+app.set("views", "./views")
+app.set("view engine", "pug")
+app.get("/tours", (req:Request, res:Response)=>{
+  res.render("client/pages/tours/index")
 })
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
