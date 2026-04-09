7004e2985db648c6b1f1da4665451870ce76a0da
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-04-09 22:30:33 +0700

    Include static files

--- index.ts
+++ index.ts
@@ -4,8 +4,12 @@ dotenv.config()
 import clientRoutes from "./routes/client/index.route"
 const app: Express = express()
 const port:number|string =  process.env.PORT || 3000
+
+app.use(express.static("public"))
+
 app.set("views", "./views")
 app.set("view engine", "pug")
+
 clientRoutes(app)
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
