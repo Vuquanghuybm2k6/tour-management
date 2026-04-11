6839e5f65167c16ed90857f89cad2ac0cdc183ab
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-04-11 23:09:09 +0700

    Build categories list page (admin)

--- index.ts
+++ index.ts
@@ -4,7 +4,8 @@ dotenv.config()
 import clientRoutes from "./routes/client/index.route"
 import bodyParser from "body-parser"
 import moment from "moment"
-
+import adminRoutes from "./routes/admin/index.route"
+import { systemConfig } from "./config/system"
 const app: Express = express()
 const port:number|string =  process.env.PORT || 3000
 app.use(bodyParser.json())
@@ -15,7 +16,10 @@ app.set("view engine", "pug")
 
 // App Locals Variables
 app.locals.moment = moment
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
 clientRoutes(app)
+adminRoutes(app)
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
