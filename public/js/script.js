b8803b1446744a673fa6c795ec15fb31a3c47d23
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-04-11 10:18:42 +0700

    Add tour to cart at localStorage (frontend)

--- public/js/script.js
+++ public/js/script.js
@@ -19,3 +19,41 @@ var imagesMain = new Swiper(".imagesMain", {
 });
 
 // End Slider Tour Detail
+
+// Cart
+
+// If there is no cart in localStorage, create a new empty cart for the user
+const cart = localStorage.getItem("cart");
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// Add tour to cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const indexExistTour = cart.findIndex(item => item.tourId == tourId);
+
+      if (indexExistTour === -1) {
+        cart.push({
+          tourId: tourId,
+          quantity: quantity
+        });
+      } else {
+        cart[indexExistTour].quantity =
+          cart[indexExistTour].quantity + quantity;
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
