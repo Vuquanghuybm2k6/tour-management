20e79f36953e753c7f07f7d0646221bde8c8de9b
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-04-11 10:37:56 +0700

    Create a mini cart on header

--- public/js/script.js
+++ public/js/script.js
@@ -44,6 +44,19 @@ if (!cart) {
   localStorage.setItem("cart", JSON.stringify([]));
 }
 
+// Display total product quantity in mini-cart
+const showMiniCart = () => {
+  const miniCart = document.querySelector("[mini-cart]");
+  if (miniCart) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+
+    miniCart.innerHTML = totalQuantity;
+  }
+}
+
+showMiniCart();
+
 // Add tour to cart
 const formAddToCart = document.querySelector("[form-add-to-cart]");
 
