1666c10f0fd94566e326369d5f4bc2e7c5300f33
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-04-11 19:29:19 +0700

    Server get data and save into orders table

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,11 +1,32 @@
 import {Request, Response} from 'express'
-import Category from '../../models/category.model'
+import Order from '../../models/order.model'
+import { generateOrderCode } from '../../helpers/generate'
 
 // [GET]: /orders
 export const order = async (req:Request, res:Response)=>{
   const data = req.body
+  // Save data into orders table
+  const dataOrder = {
+    code: "",
+    fullName: data.info.fullName,
+    phone: data.info.phone,
+    note: data.info.note,
+    status: "initial"
+  }
+  const order = await Order.create(dataOrder)
+  const orderId = order.dataValues.id
+  const code = generateOrderCode(orderId)
+  await Order.update({
+    code: code
+  },{
+    where:{
+      id: orderId
+    }
+  })
+  console.log(order)
   res.json({
     code:200,
-    message: "Đặt hàng thành công"
+    message: "Đặt hàng thành công",
+    orderCode: code
   })
 }
